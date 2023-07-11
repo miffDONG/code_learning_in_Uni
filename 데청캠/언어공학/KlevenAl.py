8cb9752f34bb0a50aa9d6f02908b2dd360443fa2
miffDONG <ksglsg1350@gmail.com>
2023-07-11 16:18:18 +0900

    2023-07-11

--- 데청캠/언어공학/KlevenAl.py
+++ 데청캠/언어공학/KlevenAl.py
@@ -1,8 +1,8 @@
 from MinEditDistance import KlevenshteinDistance
 from Kseperator import decompose
 
-str1 = '김현동'
-str2 = '김삼순'
+str1 = '하늘을 나는 새를 봤다'
+str2 = '하늘을 나는 새를 보았다'
 
 dec1 = decompose(str1)
 dec2 = decompose(str2)
@@ -10,3 +10,19 @@ dec2 = decompose(str2)
 print(dec1)
 answer = KlevenshteinDistance(dec1,dec2)
 print(answer)
+
+
+"""
++ 기능 
+
+morpheme 단위 나눌 필요성
+'하늘을 나는 새를 보았다'
+-> 하늘 을 날 는 새 를 보아 ㅆ다
+
+'하늘을 나는 새를 봤다'
+-> 하늘 을 날 는 새 를 보아 ㅆ다
+
+'하늘을 나는 새랑 봤다'
+-> 하늘 을 날 는 새 랑 보아 ㅆ다
+
+""" 

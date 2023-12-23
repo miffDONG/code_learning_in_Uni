be6d2014525eb0cdc48d962c781fe84643cc964c
miffDONG <ksglsg1350@gmail.com>
2023-12-23 19:13:30 +0900

    oriented_programming file clean

---
+++ 2-2/object_oriented programming/한류/Test.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        try {
+            
+        } catch (NumberFormatException e) {
+            // TODO: handle exception
+        }
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        System.out.println(str);
+    }
+}
